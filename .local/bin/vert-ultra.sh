897f2d7a061cfff898aa649ca35e9cd9ae662390
Amri <amri.arshad16@yahoo.com>
2024-07-22 21:30:56 +1200

    updated monitor script

--- .local/bin/vert-ultra.sh
+++ .local/bin/vert-ultra.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --primary --mode 3440x1440 --rate 144.0 --pos 1080x0 --rotate normal --output DP-2 --mode 1920x1080 --pos 0x0 --rate 60.0 --rotate right --output DP-3 --off --output HDMI-1 --off --output HDMI-1-2 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off
+xrandr --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 0x0 --rotate right --output DP-3 --primary --mode 3440x1440 --rate 120.00 --pos 1080x0 --rotate normal --output HDMI-1 --off --output HDMI-1-2 --off --output DP-1-4 --off --output DP-1-5 --off --output DP-1-6 --off
